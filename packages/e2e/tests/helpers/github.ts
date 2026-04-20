5a9404427f6a6361fd94e340719dbe7d0f98963b
Camilo Acuña <camilo.aku@gmail.com>
2026-04-20 21:52:47 +0700

    feat(e2e): add findInstallationForOrg to createGitHubCoreBackend helper

--- packages/e2e/tests/helpers/github.ts
+++ packages/e2e/tests/helpers/github.ts
@@ -22,6 +22,7 @@ export function createGitHubCoreBackend(octokit: Octokit): unknown {
     createConfigStore: async (opts: { owner: string; repo: string; ref?: string }) =>
       new GitHubConfigStore({ owner: opts.owner, repo: opts.repo, ref: opts.ref }, octokit),
     verifyInstallation: async () => true,
+    findInstallationForOrg: async () => null,
   };
 }
 
